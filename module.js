b75bb11a14bcf797ecead87c95c26476eae4beef
gongwonchong <gongwonchong@gmail.com>
2022-04-03 21:34:59 +0900

    미션 추가

--- module.js
+++ module.js
@@ -8,6 +8,7 @@ async function main()
 {
     let moduleInfo = (await getJSON("https://raw.githubusercontent.com/Kengxxiao/ArknightsGameData/master/ko_KR/gamedata/excel/uniequip_table.json"));
     let equipDict = moduleInfo["equipDict"];
+    let missionList = moduleInfo["missionList"];
 
     let table = document.createElement('table');
     let thead = document.createElement('thead');
@@ -16,6 +17,8 @@ async function main()
     var cur = 1;
     var td = [];
     var trcnt = 0;
+    var bold1 = document.createElement('b');
+    var bold2 = document.createElement('b');
     var p = document.createElement('p');
     
     // 표 만들기
@@ -34,7 +37,10 @@ async function main()
         cur_td.onclick = async function() {
             document.getElementById('title').innerText = await getOperatorName(this.id);
             var opDict = [equipDict["uniequip_001_" + this.id.split("_")[2]], equipDict["uniequip_002_" + this.id.split("_")[2]]];
+            var opCond = [missionList["uniequip_002_" + this.id.split("_")[2] + "_1"]["desc"], missionList["uniequip_002_" + this.id.split("_")[2] + "_2"]["desc"]];
             p.innerText = opDict[1]["uniEquipDesc"];
+            bold1.innerText = opCond[0] + "\n";
+            bold2.innerText = opCond[1] + "\n";
         };
         td.push(cur_td);
         if (td.length == 7 || (trcnt * 7) + td.length == Object.keys(equipDict).length / 2)
@@ -59,6 +65,8 @@ async function main()
     table.appendChild(tbody);
 
     document.getElementById('body').appendChild(table);
+    document.getElementById('body').appendChild(bold1);
+    document.getElementById('body').appendChild(bold2);
     document.getElementById('body').appendChild(p);
 }
 main();
